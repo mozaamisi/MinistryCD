f4cbb8cc417b66fcad68fb5c5abc780177e1b93d
unknown <mozaamisi>
2017-11-15 10:27:40 +0200

    feature/My_migrations

--- MinistryGen/routes/web.php
+++ MinistryGen/routes/web.php
@@ -14,11 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index',function()
+{
+    return view('index');
+});
+Route::get('/contact',function()
+{
+    return view('contact');
+});
+Route::get('/buying',function()
+{
+    return view('buying');
+});
+Route::get('/finance',function()
+{
+    return view('finance');
+});
+Route::get('/home',function()
+{
+    return view('home');
+});
 
-Route::get('/', function ()
+Route::get('/about',function()
 {
     return view('about');
 });
-//Route::ressource('/home',function(){
-//    return view('home');
-//});
+
+
